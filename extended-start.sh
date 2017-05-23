1665ddc5633e02f6c81c77ba035fef434cf517ad
György Novák <gyorgy.novak@loxxly.com>
2017-05-23 11:09:30 +0200

    Fixing parameter handling of start script

--- extended-start.sh
+++ extended-start.sh
@@ -4,4 +4,4 @@ $KAFKA_HOME/bin/zookeeper-server-start.sh -daemon $KAFKA_HOME/config/zookeeper.p
 $KAFKA_HOME/bin/kafka-server-start.sh -daemon $KAFKA_HOME/config/server.properties
 
 # run the original start script of parent fabric8/jenkins-jnlp-client image
-/usr/local/bin/start.sh
+/usr/local/bin/start.sh "$@"
